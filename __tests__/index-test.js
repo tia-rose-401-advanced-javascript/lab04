0ee90e9707ac119e10367aa13d5067a72b1b42b1
Tia Rose <shortyshort789@aol.com>
2019-05-28 21:40:00 -0700

    have tags rendering thanks to solution code. working on tests. Will complete tomorrow

---
+++ __tests__/index-test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+jest.mock('fs');
+
+const reader = require('../index.js');
+
+describe('Using Buffer Function', () => {
+
+  it('should return error when given a bad file', done => {
+    let files = ['bad.txt'];
+    reader.usingBuffer(files, (error, data) => {
+      expect(error).toBeDefined();
+      done();
+    });
+  });  
+});
